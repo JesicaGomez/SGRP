d2bb20d8b05fa83d05e906e69e9a5d4be654ee76
JesicaGomez <jesica.gomez@udea.edu.co>
2016-08-03 16:26:53 -0500

    udea

---
+++ app/assets/javascripts/principal.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("button.uno").click(function(){
+        $("div.one").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("button.dos").click(function(){
+        $("#two").toggle();
+    });
+});
